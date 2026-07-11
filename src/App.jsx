d41d2939996dca5f8e83da9ee986ef530c594a04
anan531 <ananyat612@gmail.com>
2026-07-11 10:41:53 +0530

    Delete student created'

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import AddStudent from './components/AddStudent'
 import SearchStud from './components/SearchStud'
+import DelStudent from './components/DelStudent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <>
       <AddStudent/>
       <SearchStud/>
+      <DelStudent/>
     </>
   )
 }
---
+++ src/components/DelStudent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DelStudent = () => {
+  return (
+    <div>
+<h1>Delete</h1>
+
+    </div>
+  )
+}
+
+export default DelStudent
